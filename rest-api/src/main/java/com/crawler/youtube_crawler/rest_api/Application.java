f0850588d5931768ba9b626b6421e907c1906a0d
Daria_Iurganova <eldyfraise@gmail.com>
2017-10-19 18:39:31 +0300

    get rid of redirect

--- rest-api/src/main/java/com/crawler/youtube_crawler/rest_api/Application.java
+++ rest-api/src/main/java/com/crawler/youtube_crawler/rest_api/Application.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @EnableAutoConfiguration(exclude={MultipartAutoConfiguration.class})
 @ComponentScan("com.crawler.youtube_crawler.rest_api")
 public class Application extends SpringBootServletInitializer {
-
     public static void main(final String[] arguments) throws Exception {
         SpringApplication.run(Application.class, arguments);
     }
@@ -24,10 +23,5 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
         return application.sources(Application.class);
-    }
-
-    @RequestMapping(value = {"/{path:^(?!(?:images|dist|fonts|services|css|scripts|libraries|webjars|swagger-ui.html|index.html)).*}/**/*", "/{path:^(?!(?:images|dist|fonts|services|css|scripts|libraries|webjars|swagger-ui.html|index.html)).*}"})
-    public final String redirect(@PathVariable("path") String path) {
-        return "forward:/";
     }
 }
